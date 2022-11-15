7a2747b6140bd68b1d0f58c1338161aa6f0e541c
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2022-11-15 19:04:10 +0100

    Fix autoreplant

--- src/main/java/net/trainsley69/isuck/mixin/BlockMixin.java
+++ src/main/java/net/trainsley69/isuck/mixin/BlockMixin.java
@@ -5,12 +5,13 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
 import net.trainsley69.isuck.ISuck;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,16 +22,24 @@ import static net.trainsley69.isuck.utils.AutoReplantHelper.isCrop;
 
 @Mixin(Block.class)
 public class BlockMixin {
-    @Inject(at=@At("RETURN"), method="onBreak")
-    public void afterBreak(World world, BlockPos pos, BlockState state, PlayerEntity player, CallbackInfo ci) {
-        if (!ISuck.config.AutoReplant || !world.isClient) return;
+    @Inject(at=@At("HEAD"), method="onBroken")
+    public void afterBreak(WorldAccess world, BlockPos pos, BlockState state, CallbackInfo ci) {
+        // Get the client and world
+        MinecraftClient client = MinecraftClient.getInstance();
+        World level = client.world;
+        // Return if the option isn't toggled or if it's not the client
+        if (!ISuck.config.AutoReplant || !level.isClient) return;
+        // Get the player entity and block
+        PlayerEntity player = client.player;
         Block block = state.getBlock();
+        // Check if the block is a crop, then get the main item and see if it's a valid seed
         if (block instanceof CropBlock) {
             Item item = player.getMainHandStack().getItem();
             if (!isCrop(item)) return;
-            MinecraftClient client = MinecraftClient.getInstance();
+            // Make a new BlockHitResult with the block below
             BlockHitResult oldHit = (BlockHitResult)client.crosshairTarget;
-            BlockHitResult hitMePapi = new BlockHitResult(oldHit.getPos(), Direction.UP, pos.down(), false);
+            BlockHitResult hitMePapi = new BlockHitResult(oldHit.getPos(), Direction.UP, pos.offset(Direction.Axis.Y, -1), oldHit.isInsideBlock());
+            // interact with the block using the new blockhitresult
             client.interactionManager.interactBlock(client.player, Hand.MAIN_HAND, hitMePapi);
         }
     }
