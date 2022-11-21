2a1f6d5bc1e52e3160623f4d42171af7acbb71ef
AI-nsley69 <62105998+AI-nsley69@users.noreply.github.com>
2022-11-21 21:20:29 +0100

    Update AutoFish to work when fishing with other players

--- src/main/java/net/trainsley69/isuck/mixin/FishingBobberEntityMixin.java
+++ src/main/java/net/trainsley69/isuck/mixin/FishingBobberEntityMixin.java
@@ -2,9 +2,11 @@ package net.trainsley69.isuck.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.data.TrackedData;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.util.Hand;
 import net.trainsley69.isuck.ISuck;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,11 +18,14 @@ public abstract class FishingBobberEntityMixin {
     @Shadow
     private boolean caughtFish;
 
+    @Shadow @Nullable
+    public abstract PlayerEntity getPlayerOwner();
+
     // Only pull once the fish
     @Inject(method = "onTrackedDataSet", at = @At("TAIL"))
     public void onTrackedDataSet(TrackedData<?> data, CallbackInfo ci) {
         MinecraftClient client = MinecraftClient.getInstance();
-        if (caughtFish && ISuck.config.AutoFish) {
+        if (caughtFish && ISuck.config.AutoFish && this.getPlayerOwner() == client.player) {
             client.interactionManager.interactItem(client.player, Hand.MAIN_HAND);
             ISuck.Shared.recastDelay = 6;
         }
