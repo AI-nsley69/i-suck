f85413885dacf793a34b27dec12085a83c2e3dc3
20bear <20bear@stockholmscience.se>
2022-11-30 15:57:57 +0100

    Properly render original player during freecam

--- src/main/java/net/trainsley69/isuck/mixin/WorldRendererMixin.java
+++ src/main/java/net/trainsley69/isuck/mixin/WorldRendererMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.entity.Entity;
 import net.minecraft.util.math.Matrix4f;
 import net.minecraft.util.math.Vec3d;
 import net.trainsley69.isuck.ISuck;
-import net.trainsley69.isuck.utils.FakePlayer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,14 +18,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class WorldRendererMixin {
     // From https://github.com/hashalite/Freecam/blob/main/src/main/java/net/xolt/freecam/mixins/WorldRendererMixin.java
     // Shows the original player
-    @Shadow
-    private BufferBuilderStorage bufferBuilders;
+    @Shadow @Final private BufferBuilderStorage bufferBuilders;
 
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;checkEmpty(Lnet/minecraft/client/util/math/MatrixStack;)V", ordinal = 0))
     private void onRender(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
         if (shouldRenderPlayer()) {
-            Vec3d fakePos = FakePlayer.getInstance().getPos();
-            renderEntity(MinecraftClient.getInstance().player, fakePos.x, fakePos.y, fakePos.z, tickDelta, matrices, bufferBuilders.getEntityVertexConsumers());
+            Vec3d cameraPos = camera.getPos();
+            renderEntity(MinecraftClient.getInstance().player, cameraPos.x, cameraPos.y, cameraPos.z, tickDelta, matrices, bufferBuilders.getEntityVertexConsumers());
         }
     }
 
